4cec134e99e4a99010efdefcefc84afef0940ab3
Ashish Gaikwad <mr.ashishgg@gmail.com>
2026-09-03 21:52:05 +0530

    parallel testing's testng.xml

--- src/test/java/testRunners/TestRunner.java
+++ src/test/java/testRunners/TestRunner.java
@@ -1,17 +1,45 @@
+//package testRunners;
+//
+//import io.cucumber.core.cli.Main;
+//
+//public class TestRunner {
+//
+//	public static void main(String[] args) {
+//
+//		Main.main(new String[]{
+//				"--glue", "stepDefinitions",
+//				"--glue", "AppHooks",
+//				"src/test/resources/com/AppFeatures",
+//				"--plugin", "pretty",
+//				"--plugin", "rerun:target/rerun.txt"
+//		});
+//	}
+//}
+
 package testRunners;
 
-import io.cucumber.core.cli.Main;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
-public class TestRunner {
-
-	public static void main(String[] args) {
-
-		Main.main(new String[]{
-				"--glue", "stepDefinitions",
-				"--glue", "AppHooks",
-				"src/test/resources/com/AppFeatures",
-				"--plugin", "pretty",
-				"--plugin", "rerun:target/rerun.txt"
-		});
+@CucumberOptions(
+		features = "src/test/resources/com/AppFeatures",
+		glue = {
+				"stepDefinitions",
+				"AppHooks"
+		},
+		plugin = {
+				"pretty",
+				"rerun:target/rerun.txt",
+				"html:target/cucumber-report.html",
+//				"io.qameta.allure.testng.AllureTestNg"
+		},
+		monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
 	}
 }
